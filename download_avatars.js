76c043c1e2f438e3b169f4fe2438202257e22a42
Nikolai <nikolaigauer@gmail.com>
2017-07-04 04:15:00 +0000

    modified some functions

--- download_avatars.js
+++ download_avatars.js
@@ -1,19 +1,81 @@
+var https = require('https');
 var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER = process.env.GITHUB_USER;
 var GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 
-var repoName = "jquery"
-var repoOwner = "jquery"
+// var requestOptions = {
+//   host: 'api.github.com',
+//   path: '/@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors'
+// }
 
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-console.log(requestURL);
+// function getRequestOptions(path) {
+//   return {
+//     url: 'https://api.github.com' + path,
+//     headers: {
+//       'User-Agent': 'Bobby Conn'
+//     },
+//     qs: {
+//       access_token: GITHUB_TOKEN
+//     }
+//   };
+// }
+
+// var repoName = "jquery"
+// var repoOwner = "jquery"
+
+
+function getRequestOptions(repoOwner, repoName) {
+  return {
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'kittenfingers'
+    },
+  };
+}
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  getRepoContributors("jquery", "jquery", function(err, result) {
+  request(getRequestOptions(repoOwner, repoName), function (error, response, body) {
+    try {
+      const data = JSON.parse(body);
+      console.log(data);
+    } catch (err) {
+      console.log('Failed to parse content body');
+    }
+  });
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
-  // ...
-}
+
+
+
+
+// function getRequest(requestURL) {
+//   console.log(requestURL);
+// }
+
+// var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+// // console.log(requestURL);
+
+// function getRepoContributors(repoOwner, repoName, cb) {
+
+//   https.get(repoOwner,)
+//   getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   console.log("Result:", result);
+// });
+//   // ...
+
+//   var requestOptions = {
+//   host: 'api.github.com',
+//   path: '/@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors'
+
+// };
+
+// return getRepoContributors("jquery", "jquery", getRequest);
