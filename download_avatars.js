5cff1a16f88a4d1be6ad5c8cadd8a701204fbff3
Nikolai <nikolaigauer@gmail.com>
2017-07-04 20:56:52 +0000

    cleaned up the code a bit. Added more precise error messages

--- download_avatars.js
+++ download_avatars.js
@@ -3,8 +3,8 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 var fs = require('fs');
 
-var GITHUB_USER = process.env.GITHUB_USER;
-var GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
+// var GITHUB_USER = process.env.GITHUB_USER;
+// var GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 
 // var GITHUB_USER = process.argv[2]
 // var GITHUB_TOKEN = process.env[3]
@@ -20,28 +20,41 @@ function getRequestOptions(repoOwner, repoName) {
 }
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  request(getRequestOptions(repoOwner, repoName), function (error, response, body) {
-    try {
-      const data = JSON.parse(body);
-
-      // console.log(data);
-      cb(null, data)
-
-    } catch (err) {
-      console.log('Failed to parse content body');
-      cb(err, null);
-    }
-  });
+  if (!repoOwner || !repoName) {
+    // console.error("YOU DID NOT PUT IN A VALID OWNER OR REPO NAME")
+    cb(new Error('YOU DID NOT PUT IN A VALID OWNER OR REPO NAME'), null);
+  } else {
+    request(getRequestOptions(repoOwner, repoName), function (error, response, body) {
+      if (error) {
+        cb(error, null);
+        // console.log('error found in getRequest', error);
+      } else {
+        try {
+          console.log('body:', response.statusCode);
+          if (response.statusCode === 200) {
+            // Only if we get a success HTTP status, then we do something with the response
+            const data = JSON.parse(body);
+            // console.log(data);
+            cb(null, data);
+          } else {
+            //
+            cb(new Error(response.statusCode + ' ' + response.statusMessage), null);
+          }
+        } catch (parseErr) {
+          // console.log('Failed to parse content body', err);
+          cb(parseErr, null);
+        }
+      }
+    });
+  }
 }
-
-
-
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .on('error', function (err) {
       console.log("ERROR ERROR ERROR!!!! OH DEAR ME, WE SEEM TO HAVE AN ERROR")                                // Note 2
     throw err;
+
     })
     .on('response', function (response) {                           // Note 3
       console.log('Response Status Code: ', response.statusCode, 'CHECK THE STATUS: ', response.statusMessage, response.headers['THIS IS THE CONTENT TYPE!'])
@@ -49,14 +62,18 @@ function downloadImageByURL(url, filePath) {
     .pipe(fs.createWriteStream(filePath));
 }
 
+function processContributors(contributor) {
+  downloadImageByURL(contributor.avatar_url, `avatars/${contributor.login}.jpg` ) // <------- invokes the downloadImageByURL function with the URL specified in .avatar_url in 'data'
+}
+
 getRepoContributors(process.argv[2], process.argv[3], function(err, contributors) {
 
-  console.log("Errors:", err);
-  // console.log("Result:", contributors);          <---------- prints the list of the 'data'
-  contributors.forEach((contributor) => {         //<---------- sifts through the 'data'
-     // console.log(contributor.avatar_url);
-    downloadImageByURL(contributor.avatar_url, `avatars/${contributor.login}.jpg` ) // <------- invokes the downloadImageByURL function with the URL specified in .avatar_url in 'data'
-  });
+  if (err) {
+    console.log("Errors:", err);
+  } else if (contributors) {
+    // console.log("Result:", contributors);          <---------- prints the list of the 'data'
+    contributors.forEach(processContributors);
+  }
 
 
 });
