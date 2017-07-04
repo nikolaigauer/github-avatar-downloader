7ab74ebeb300744fe184e15e284d0cf51fcb6167
Nikolai <nikolaigauer@gmail.com>
2017-07-04 02:56:52 +0000

    added jquery as repo name and repo owner

--- download_avatars.js
+++ download_avatars.js
@@ -4,6 +4,9 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 var GITHUB_USER = process.env.GITHUB_USER;
 var GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 
+var repoName = "jquery"
+var repoOwner = "jquery"
+
 var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 console.log(requestURL);
 
