1765974b484b5150c8285d97c599b5fdb55fe0d8
Nikolai <nikolaigauer@gmail.com>
2017-07-04 04:52:04 +0000

    Cleaned up the code and taken away nonused comments/code for better readability

--- download_avatars.js
+++ download_avatars.js
@@ -5,27 +5,6 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 var GITHUB_USER = process.env.GITHUB_USER;
 var GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 
-// var requestOptions = {
-//   host: 'api.github.com',
-//   path: '/@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors'
-// }
-
-// function getRequestOptions(path) {
-//   return {
-//     url: 'https://api.github.com' + path,
-//     headers: {
-//       'User-Agent': 'Bobby Conn'
-//     },
-//     qs: {
-//       access_token: GITHUB_TOKEN
-//     }
-//   };
-// }
-
-// var repoName = "jquery"
-// var repoOwner = "jquery"
-
-
 function getRequestOptions(repoOwner, repoName) {
   return {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
@@ -64,27 +43,3 @@ getRepoContributors("jquery", "jquery", function(err, result) {
 
 
 
-
-// function getRequest(requestURL) {
-//   console.log(requestURL);
-// }
-
-// var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-// // console.log(requestURL);
-
-// function getRepoContributors(repoOwner, repoName, cb) {
-
-//   https.get(repoOwner,)
-//   getRepoContributors("jquery", "jquery", function(err, result) {
-//   console.log("Errors:", err);
-//   console.log("Result:", result);
-// });
-//   // ...
-
-//   var requestOptions = {
-//   host: 'api.github.com',
-//   path: '/@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors'
-
-// };
-
-// return getRepoContributors("jquery", "jquery", getRequest);
