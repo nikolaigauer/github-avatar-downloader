3a1668b76d6499b1fd8eb4f5b465991d577633c7
Nikolai <nikolaigauer@gmail.com>
2017-07-04 02:30:17 +0000

    added reference to my github token and username, which are not included but established in my environment

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,11 @@
 var request = require('request');
-
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+var GITHUB_USER = process.env.GITHUB_USER;
+var GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
+
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
 
 function getRepoContributors(repoOwner, repoName, cb) {
   getRepoContributors("jquery", "jquery", function(err, result) {
