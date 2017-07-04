9e43d398437a78d07b41c8a3a617488833d14a9e
Nikolai <nikolaigauer@gmail.com>
2017-07-04 19:35:39 +0000

    bound the functions to download the avatar images

--- download_avatars.js
+++ download_avatars.js
@@ -7,39 +7,57 @@ var GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 
 function getRequestOptions(repoOwner, repoName) {
   return {
-    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
       'User-Agent': 'Bobby Conn'
     },
   };
 }
 
-
+var login = [];
+var avatarURL = [];
 function getRepoContributors(repoOwner, repoName, cb) {
   request(getRequestOptions(repoOwner, repoName), function (error, response, body) {
     try {
       const data = JSON.parse(body);
 
       // console.log(data);
-
-       data.forEach((contributor) => {
-    console.log(contributor.avatar_url);
-      });
-
+      cb(null, data)
 
     } catch (err) {
       console.log('Failed to parse content body');
-        }
-
-
+      cb(err, null);
+    }
   });
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, contributors) {
+
   console.log("Errors:", err);
-  console.log("Result:", result);
+  console.log("Result:", contributors);
+  contributors.forEach((contributor) => {
+     // console.log(contributor.avatar_url);
+     downloadImageByURL(contributor.avatar_url, `avatars/${contributor.login}.jpg` )
+      });
+
+
 });
 
 
 
+  var request = require('request');
+  var fs = require('fs');
+
+
+function downloadImageByURL(url, filePath) {
+    request.get(url)
+        .on('error', function (err) {
+          console.log("ERROR ERROR ERROR!!!! OH DEAR ME, WE SEEM TO HAVE AN ERROR")                                // Note 2
+          throw err;
+        })
+        .on('response', function (response) {                           // Note 3
+          console.log('Response Status Code: ', response.statusCode, 'CHECK THE STATUS: ', response.statusMessage, response.headers['THIS IS THE CONTENT TYPE!'])
+        })
+       .pipe(fs.createWriteStream(filePath));
+}
